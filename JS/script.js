0b2aad97d824ea0dfd9be7f0c46d599f20d6e379
CyberRoshan <roshanchaurasia990@gmail.com>
2024-09-18 17:28:56 +0530

    animation added

--- JS/script.js
+++ JS/script.js
@@ -67,3 +67,23 @@ $("#zenviWorks").owlCarousel({
     },
   },
 });
+
+// $("#testimonialSlider").owlCarousel({
+//   loop: true,
+//   margin: 30,
+//   nav: false,
+//   autoplay: true,
+//   autoplaySpeed: 1000,
+
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     600: {
+//       items: 2,
+//     },
+//     1000: {
+//       items: 3,
+//     },
+//   },
+// });
